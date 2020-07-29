e0ea14b521b2969210a9c83c46f8a86ad1b8c78c
VGB <vgboy@pm.me>
2020-07-28 17:58:32 -0700

    Added listview builder and task class

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todoey/models/task.dart';
 import 'package:flutter_todoey/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milke'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
-    );
+    return ListView.builder(itemBuilder: (context, index) {
+      return TaskTile(
+        taskTitle: tasks[index].name,
+        isChecked: tasks[index].isDone,
+        checkboxCallback: (bool checkboxState) {
+          setState(() {
+            tasks[index].toggleDone();
+          });
+        },
+
+      );
+    }, itemCount: tasks.length);
   }
 }
