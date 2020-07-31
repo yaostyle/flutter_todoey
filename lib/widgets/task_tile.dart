b8df4def5cf5f8f59f1ecfbc407c2363b2646cbf
VGB <vgboy@pm.me>
2020-07-31 09:45:39 -0700

    Added delete task function

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,11 +4,18 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final Function checkboxCallback;
-  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+  final Function longPressCallback;
+
+  TaskTile(
+      {this.isChecked,
+      this.taskTitle,
+      this.checkboxCallback,
+      this.longPressCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: longPressCallback,
       title: Text(
         taskTitle,
         style: isChecked
@@ -23,5 +30,3 @@ class TaskTile extends StatelessWidget {
     );
   }
 }
-
-
